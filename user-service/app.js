71f3e055f30b8d4090fa5a95781fee3ef5dca4cc
vamshi-rdy <72117135+vamshi-rdy@users.noreply.github.com>
2025-08-23 18:10:34 +0530

    Update app.js

--- user-service/app.js
+++ user-service/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const userRoutes = require('./routes/userRoutes');
+// const userRoutes = require('./routes/userRoutes');  // uncomment ,if endpoints are used
 
-app.use('/api/users', userRoutes);
+// app.use('/api/users', userRoutes);  // uncomment ,if endpoints are used
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
